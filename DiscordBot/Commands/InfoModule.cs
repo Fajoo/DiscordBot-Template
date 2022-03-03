9b506c89dc1a79e5435fc245aabc58373ee025de
Fajo <dimasav57@gmail.com>
2022-03-03 16:50:29 +0300

    Добавьте файлы проекта.

---
+++ DiscordBot/Commands/InfoModule.cs
@@ -0,0 +1,11 @@
+﻿using Discord.Commands;
+
+namespace DiscordBot.Commands;
+
+public class InfoModule : ModuleBase<SocketCommandContext>
+{
+	[Command("say")]
+    [Discord.Commands.Summary("Echoes a message.")]
+    public Task SayAsync([Remainder][Discord.Commands.Summary("The text to echo")] string echo)
+        => ReplyAsync(echo);
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordBot.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace DiscordBot;
+
+class Program
+{
+    // Program entry point
+    static async Task Main(string[] args)
+    {
+        var logging = new LoggerConfiguration()
+            .MinimumLevel.Override("DiscordBot", LogEventLevel.Information)
+            .WriteTo.File("DiscordBotLog-.txt", rollingInterval:
+                RollingInterval.Day)
+            .CreateLogger();
+
+        var host = CreateHostBuilder(args).Build();
+
+        await host.RunAsync();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureDiscordHost((context, config) =>
+            {
+                config.SocketConfig = new DiscordSocketConfig
+                {
+                    LogLevel = LogSeverity.Info,
+                    AlwaysDownloadUsers = true,
+                    MessageCacheSize = 200
+                };
+
+                config.Token = context.Configuration["token"];
+            })
+            .UseSerilog((context, configuration) => configuration
+                        .Enrich.FromLogContext()
+                        .MinimumLevel.Information()
+                        .WriteTo.Console(),
+                    preserveStaticLogger: true)
+            .UseCommandService((context, config) =>
+            {
+                config.DefaultRunMode = RunMode.Async;
+                config.CaseSensitiveCommands = false;
+            })
+            .UseInteractionService((context, config) =>
+            {
+                config.LogLevel = LogSeverity.Info;
+                config.UseCompiledLambda = true;
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHostedService<CommandHandler>();
+
+                //Add any other services here
+                services.AddHttpClient();
+                services.AddHostedService<BotStatusService>();
+            });
+}
